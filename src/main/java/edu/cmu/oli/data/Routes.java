e9a0ea42a5e6e3ab9d5d7365be80a03d16cb35d3
Raphael Gachuhi <rgachuhi@cmu.edu>
2021-02-26 14:58:49 -0500

    add api token

--- src/main/java/edu/cmu/oli/data/Routes.java
+++ src/main/java/edu/cmu/oli/data/Routes.java
@@ -9,6 +9,7 @@ import io.smallrye.mutiny.Uni;
 import io.vertx.core.http.HttpMethod;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.RoutingContext;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
@@ -16,8 +17,6 @@ import javax.inject.Inject;
 import javax.validation.Valid;
 import javax.ws.rs.core.Response;
 import java.util.Map;
-
-import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 @RouteBase(path = "cloud")
@@ -35,7 +34,7 @@ public class Routes {
     @Route(path = "/quiz/data", methods = HttpMethod.POST)
     Uni<JsonObject> processQuizData(RoutingContext rc, @Body @Valid QuizDetailsForm formData) {
         String token = rc.request().headers().get("api_token");
-        if(token == null || !token.equals(apiToken)){
+        if (token == null || !token.equals(apiToken)) {
             rc.response().setStatusCode(Response.Status.FORBIDDEN.getStatusCode());
             return Uni.createFrom().item(new JsonObject(Map.of("error", "forbidden")));
         }
